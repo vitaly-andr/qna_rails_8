d3fafe1c1741954f88297945c272eb87b3e8a9c5
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-28 22:12:13 +0300

    Added postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'qna8.andrianoff.online'}
   config.action_mailer.default_options = {from: 'QNA <noreply@andrianoff.online>'}
 
-  config.hosts << "qna8.andrianoff.online"
-  config.hosts << "00b04961405a"
+  # config.hosts << "qna8.andrianoff.online"
+  # config.hosts << "00b04961405a"
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
